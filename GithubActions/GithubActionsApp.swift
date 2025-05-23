ad89406ed2a38d4ca1956582140563d2f720b96f
AgataPrzykaza <agataprzykaza89@gmail.com>
2025-05-23 13:12:13 +0200

    Initial Commit

---
+++ GithubActions/GithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsApp.swift
+//  GithubActions
+//
+//  Created by Agata Przykaza on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubActionsTests/GithubActionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsTests.swift
+//  GithubActionsTests
+//
+//  Created by Agata Przykaza on 23/05/2025.
+//
+
+import Testing
+@testable import GithubActions
+
+struct GithubActionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
